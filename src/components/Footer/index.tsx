3875b9a5773f51bcd0a36c9c2576be1baac3305d
abhisektomar1 <abhisektomar1@gmail.com>
2024-08-30 13:00:32 +0530

    adding button for privacy policy

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,26 +1,30 @@
+import { Link } from "react-router-dom";
 import logoWhite from "../../assets/images/logow.png";
 const Footer = () => {
-	// const navLinks = [
-	// 	{ title: "Home", path: "/", active: true },
-	// 	{ title: "Services", path: "/", active: false },
-	// 	{ title: "Products", path: "/", active: false },
-	// 	{ title: "Newsletter", path: "/", active: false },
-	// 	{ title: "About", path: "/", active: false },
-	// ];
-	return (
-		<div className="w-full bg-[#263640] md:px-40 px-5 py-10 flex md:flex-row flex-col md:gap-0 gap-8 items-center justify-between">
-			<img
-				src={logoWhite}
-				alt="logo"
-				style={{width:"300px"}}
-			/>
-			<div className="flex items-center gap-5 text-gray-300">
-				{/* {navLinks.map((link) => {
-					return <span className="font-semibold text-white cursor-pointer">{link.title}</span>;
-				})} */}
-				@Copyright AgriSarathi 2024 			</div>
-		</div>
-	);
+  const navLinks = [
+    { title: "Home", path: "/", active: true },
+    { title: "Privacy Policies", path: "/privacyPolicy", active: false },
+    // { title: "Products", path: "/", active: false },
+    // { title: "Newsletter", path: "/", active: false },
+    // { title: "About", path: "/", active: false },
+  ];
+  return (
+    <div className="w-full bg-[#263640] md:px-40 px-5 py-10 flex md:flex-row flex-col md:gap-0 gap-8 items-center justify-between">
+      <img src={logoWhite} alt="logo" style={{ width: "300px" }} />
+      <div className="flex items-center gap-5 text-gray-300">
+        {navLinks.map((link) => {
+          return (
+			<Link to={link.path}>
+            <span className="font-semibold text-white cursor-pointer">
+              {link.title}
+            </span>
+			</Link>
+          );
+        })}
+        @Copyright AgriSarathi 2024{" "}
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
